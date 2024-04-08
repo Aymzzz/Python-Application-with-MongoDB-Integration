18b03e7ed8f25dee0700c285d71e6bb2384be429
Aymzzz <sbaiaymane6@gmail.com>
2024-04-08 01:34:23 +0000

    Added more files and code

--- CRUD_Operations.py
+++ CRUD_Operations.py
@@ -1,1 +1,41 @@
-import PyMongo
+import pymongo
+from pymongo import MongoClient, errors
+import importlib
+import inspect
+
+try:
+    client = MongoClient()  # normally it should be 'mongodb://localhost:27017/'
+
+    db_name = 'Project-Sensors'
+    database = client[db_name]
+    
+    print("Database connection has been established!")
+    
+    def insert_data(collection_name, data): # insert data into database
+        try:
+            collection = database[collection_name]
+            inserted_id = collection.insert_one(data).inserted_id
+            print(f"Data inserted with ID: {inserted_id}")
+        except Exception as e:
+            print(f"Error: {e}")
+    
+    def list_databases(): # list available databases
+        databases = client.list_database_names()
+        print("Available databases:")
+        for db in databases:
+            print(db)
+    
+    
+    def list_collections(db_name): # list collections in a database
+        db = client[db_name]
+        collections = db.list_collection_names()
+        print(f"Collections in database '{db_name}':")
+        for collection in collections:
+            print(collection)
+
+    
+except (pymongo.errors.ConnectionFailure, pymongo.errors.ServerSelectionTimeoutError) as e:
+    print(f"An error occurred while connecting to MongoDB: {e}")
+    
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
---
+++ main.py
@@ -0,0 +1,53 @@
+from pymongo import MongoClient
+from CRUD_Operations import insert_data
+import CRUD_Operations
+import inspect
+import json
+
+try:
+    client = MongoClient()  # normally it should be 'mongodb://localhost:27017/'
+
+    selected_db = None
+    while not selected_db: #asking the user to choose the database available
+        print("Available databases:")
+        databases = client.list_database_names()
+        i = 0
+        for db in databases:
+            i += 1
+            print(f"{i}. {db}")
+        selected_db = input("Select a database: ")
+
+    database = client[selected_db]
+    print(f"Selected database: {selected_db}")
+
+    selected_collection = None
+    while not selected_collection: #asking the user to choose the corresponding collection
+        print(f"Available collections in {selected_db}:")
+        collections = database.list_collection_names()
+        for collection in collections:
+            print(collection)
+        selected_collection = input("Select a collection: ")
+
+    print(f"Selected collection: {selected_collection}")
+
+    available_functions = [name for name, obj in inspect.getmembers(CRUD_Operations) if inspect.isfunction(obj)]
+        
+    print("Available functions:") # print available functions
+    for function_name in available_functions: 
+        print(function_name)
+
+    selected_function = input("Select a function: ")
+    if selected_function in available_functions:
+        
+        if selected_function == "insert_data": # Prompt for additional arguments based on the selected function
+            data_path = input("Enter the path of the data file: ")
+            with open(data_path) as file:
+                data = json.load(file)
+            insert_data(data, selected_db, selected_collection) # we pass those arguments to the functios
+        else:
+            print("Invalid function name!")
+    else:
+        print("Function not found!")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
