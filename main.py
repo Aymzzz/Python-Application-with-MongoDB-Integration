9eecefec66aa382e020a19a2ac98847fe0efe2d1
Alloul-Badreddine <be.alloul@gmail.com>
2024-04-20 16:40:29 +0100

    main.py that works for badr

--- main.py
+++ main.py
@@ -1,172 +1,68 @@
-import os
+from pymongo import MongoClient
+from CRUD_Operations import delete_data, insert_data, read_data, create_data, update_data
+import CRUD_Operations
+import inspect
 import json
-from CRUD_Operations import *
 
-# This part is about preparing certain functions that will help us during the execution of the operations.
-def print_menu(): #prints the available functions
-    print("\nMenu:")
-    print("1. Insert JSON document into database")
-    print("2. CRUD Operations")
-    print("3. Sort Data")
-    print("0. Exit")
+try:
+    client = MongoClient()  # normally it should be 'mongodb://localhost:27017/'
 
-def check_mongodb_service():
-    #Check MongoDB service status 
-    status = os.system("systemctl is-active --quiet mongod")
-    if status == 0:
-        print("MongoDB service is running.")
-    else:
-        print("MongoDB service is not running. Starting the service...")
-        os.system("sudo systemctl start mongod") #to start the service for the user
-        print("MongoDB service has been started.")
+    selected_db = None
+    while not selected_db: #asking the user to choose the database available
+        print("Available databases:")
+        databases = client.list_database_names()
+        for i, db in enumerate(databases, 1):
+            print(f"{i}. {db}")
+        selected_db_index = int(input("Select a database number: ")) - 1
+        selected_db = databases[selected_db_index]
 
-def list_databases(): # lists all databases
-    print("\nAvailable databases:")
-    databases = get_databases()
-    for i, db in enumerate(databases, start=1):
-        print(f"{i}. {db}")
-    return databases
+    print(f"Selected database: {selected_db}")
 
-def list_collections(database_name): #lists all collections
-    print("\nAvailable collections:")
-    collections = get_collections(database_name)
-    for i, collection in enumerate(collections, start=1):
+    database = client[selected_db]
+
+    print(f"Available collections in {selected_db}:")
+    collections = database.list_collection_names()
+    for i, collection in enumerate(collections, 1):
         print(f"{i}. {collection}")
-    return collections
 
-def beautify_json(data): #cleans the output
-    for doc in data:
-        if '_id' in doc:
-            doc['_id'] = str(doc['_id'])
-    return json.dumps(data, indent=4)
-    
-'''
-This upcoming part will be provide the user with a Menu full of possible operations to be performed on the database.
-These operations include: Creating, Reading, Updating and Deleting data as part of CRUD operations.
-Data insertion from a given JSON file is also included as an option to quickly populate the collection withint the database.
-The operations will prompt the user to enter the chosen database and collection.
-'''
-def main():
-    
-    check_mongodb_service() # checking the mongodb service if it is available
-    databases = list_databases() #lists the databases to give the user an idea of the existing ones.
-    
-    #this next part will ask the user if they would like to create a new database or use an existing one;
-    create_db = input("\nDo you want to create a new database? (Type 'n' if you want to choose an existing one) (y/n): ")
+    selected_collection_index = int(input("Select a collection number: ")) - 1
+    if selected_collection_index < 0 or selected_collection_index >= len(collections):
+        raise ValueError("Invalid collection number!")
+    selected_collection = collections[selected_collection_index]
 
-    if create_db.lower() == "y":
-        while True:
-            print("\nCreating New Database:")
-            database_name = input("Enter the name of the new database: ")
-            if database_name in databases:
-                print(f"database {database_name} already exists! Enter a new name.")
-            else:
-                create_database(database_name)
-                collection_name = input("Enter the name of the collection: ")
-                create_collection(database_name, collection_name)
-                break
+    print(f"Selected collection: {selected_collection}")
+
+    available_functions = [name for name, obj in inspect.getmembers(CRUD_Operations) if inspect.isfunction(obj)]
         
-    elif create_db.lower() == "n":
-        # Validate the user's input for the database and collection names
-        
-        while True:
+    print("Available functions:") # print available functions
+    for i, function_name in enumerate(available_functions, 1): 
+        print(f"{i}. {function_name}")
 
-            database_name = input("Enter the name of the database: ")
-            if database_name not in databases:
-                print(f"Database {database_name} does not exist. Please try again.")
-                continue
-            else:
-                collections = list_collections(database_name) #if the input correct then the collections show
-                
-                while True: #similarly, we do the same thing for collections
-                    collection_name = input("Enter the name of the collection: ")
-                    if collection_name not in collections:
-                        print(f"Collection {collection_name} does not exist in database {database_name}. Please try again.")
-                    else:
-                        break
-    
-                if collection_name in collections:
-                    break
-    else:
-        print("Invalid choice. Please try again.")
-    
-    while True: # Once everything is good, we can move to this step!!
-        
-        print_menu()
-        
-        database = MongoClient()[database_name]    
-        
-        # Possible operations are, as mentioned above, the following:       
-        choice = input("Enter your choice: ")
-   
-        # and now for every choice its implementation from the CRUD_operations.py
-        if choice == "1":
-                file_path = input("Enter the path of the data file: ") 
-                insert_data(database, collection_name, file_path)
+    selected_function_index = int(input("Select a function number: ")) - 1
+    selected_function = available_functions[selected_function_index]
 
-        elif choice == "2":
-            print("\nCRUD Operations:")
-            if not databases:
-                print("No databases available. Please create a database first.")
-            else:
-                print("1. Create Data")
-                print("2. Read Data")
-                print("3. Update Data")
-                print("4. Delete Data")
-                
-                crud_choice = input("Enter your choice: ")
-                
-                if crud_choice == "1":
-                    create_data(database, collection_name)
-                    
-                elif crud_choice == "2":
-                    collection = database[collection_name]
-                    document = collection.find_one()  # retrieve one sample from the collection, the reason is to get the keys to display for the user.
+    match selected_function:
+        case "insert_data":
+            file_path = input("Enter the path of the data file: ")
+            insert_data(database, selected_collection, file_path)
+            
+        case "create_data":
+            create_data(database, selected_collection)
+            
+        case "read_data":
+            criteria = input("Enter the criteria: ")
+            read_data(database, selected_collection, criteria)
+            
+        case "update_data":
+            update_data(database, selected_collection)
+            
+        case "delete_data":
+            delete_data(database, selected_collection)
+            
+        case _:
+            print("Invalid function name!")
 
-                    if document:
-                        available_fields = list(document.keys())
-                        print("Available fields: ", available_fields)
-                                            
-                    criteria = input("\n ==> Enter the criteria (leave empty to retrieve all): ")
-                    result = read_data(database, collection_name, criteria)
-                    if result:
-                        print("Retrieved data:")
-                        print(beautify_json(result))
-                        
-                elif crud_choice == "3":
-                    collection = database[collection_name]
-                    document = collection.find_one()  # retrieve one sample from the collection, the reason is to get the keys to display for the user.
-
-                    if document:
-                        available_fields = list(document.keys())
-                        print("Available fields: ", available_fields)
-                    update_data(database, collection_name)
-                    
-                elif crud_choice == "4":
-                    collection = database[collection_name]
-                    document = collection.find_one()  # retrieve one sample from the collection, the reason is to get the keys to display for the user.
-
-                    if document:
-                        available_fields = list(document.keys())
-                        print("Available fields: ", available_fields)
-                        
-                    delete_data(database, collection_name)
-                else:
-                    print("Invalid choice. Please try again.")
-
-        elif choice == "3":
-            print("\nSorting Data:")
-            database = MongoClient()[database_name]
-            result = sorting_algorithm(database[collection_name])
-            if result:
-                print("Sorted data:")
-                print(beautify_json(result))
-
-        elif choice == "0": # once the user is done, they can exit.
-            print("Exiting...")
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-if __name__ == "__main__":
-    main()
+except ValueError as ve:
+    print(f"Error: {ve}")
+except Exception as e:
+    print(f"An error occurred: {e}")
